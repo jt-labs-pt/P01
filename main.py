dbe4087a1f77cc1c064f49851e54377b245c6b54
jt-labs-pt <josemctomaz@gmail.com>
2026-01-17 09:32:27 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+Money=input ("Money:")
+article_1=input ("1st article:")
+article_2=input ("2nd article:")
+article_3=input ("3rd article:")
+article_4=input ("4th article:")
+article_5=input ("5th article:")
+articles=article_1+article_2+article_3+article_4+article_5
+Anwser=articles-Money
+print(Anwser)
